9660363c3a69d37008cfa985c6a0828148ee5a90
guzmaacevedo <guzmaacevedo@gmail.com>
2025-12-30 14:37:58 -0500

    actualizar app.js

--- js/app.js
+++ js/app.js
@@ -3,13 +3,13 @@
 // =======================
 const productos = [
   { id: 1, nombre: "Six pack- Andina light 269ml + hielo", precio: 3000, imagen: "assets/img/andina_p.jpg" },
-  { id: 2, nombre: "Six pack- Aguila light 269ml + hielo", precio: 3000, imagen: "../assets/img/light-p.jpg" },
-  { id: 3, nombre: "Six pack- Aguila Original 269ml + hielo", precio: 3000, imagen: "../assets/img/aguila-p.jpg" },
-  { id: 4, nombre: "Six pack- Coronita 210ml + hielo", precio: 3000, imagen: "../assets/img/coronita-p.jpg" },
-  { id: 5, nombre: "Six pack- Stella Artois 330ml + hielo", precio: 3500, imagen: "../assets/img/stella-p.jpeg" },
-  { id: 6, nombre: "Six pack- Heineken 250ml + hielo", precio: 3500, imagen: "../assets/img/heineken.jpeg" },
-  { id: 7, nombre: "Six pack- Costeña Bacana 330ml + hielo", precio: 3500, imagen: "../assets/img/costeña.png" },
-  { id: 8, nombre: "Whisky Buchanans Deluxe 12años + hielo + servicio", precio: 3500, imagen: "../assets/img/buchana.png" }
+  { id: 2, nombre: "Six pack- Aguila light 269ml + hielo", precio: 3000, imagen: "assets/img/light-p.jpg" },
+  { id: 3, nombre: "Six pack- Aguila Original 269ml + hielo", precio: 3000, imagen: "assets/img/aguila-p.jpg" },
+  { id: 4, nombre: "Six pack- Coronita 210ml + hielo", precio: 3000, imagen: "assets/img/coronita-p.jpg" },
+  { id: 5, nombre: "Six pack- Stella Artois 330ml + hielo", precio: 3500, imagen: "assets/img/stella-p.jpeg" },
+  { id: 6, nombre: "Six pack- Heineken 250ml + hielo", precio: 3500, imagen: "assets/img/heineken.jpeg" },
+  { id: 7, nombre: "Six pack- Costeña Bacana 330ml + hielo", precio: 3500, imagen: "assets/img/costeña.png" },
+  { id: 8, nombre: "Whisky Buchanans Deluxe 12años + hielo + servicio", precio: 3500, imagen: "assets/img/buchana.png" }
 
   //{ id: 8, nombre: "Six pack- Stella lata 330ml + hielo", precio: 3500, imagen: "../assets/img/stella-l.jpeg" },//
 ];
@@ -153,5 +153,6 @@ function formatearPrecio(valor) {
 
 // INICIAR
 mostrarProductos();
+
 
 
