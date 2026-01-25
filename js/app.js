82c4cc0335d354de111526ed2270b58f506b9090
guzmaacevedo <guzmaacevedo@gmail.com>
2026-01-24 21:51:48 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const productos = [
   //{ id: 1, nombre: "Six pack- Andina light 269ml + hielo", precio: 12500, imagen: "assets/img/andina_p.jpg" },
   { id: 2, nombre: "Six pack- Aguila light 269ml + hielo", precio: 13450, imagen: "assets/img/light-p.jpg" },
   { id: 3, nombre: "Six pack- Aguila Original 269ml + hielo", precio: 16250, imagen: "assets/img/aguila-p.jpg" },
-  { id: 4, nombre: "Six pack- Coronita 210ml + hielo", precio: 22499, imagen: "assets/img/coronita-p.jpg" },
+  //{ id: 4, nombre: "Six pack- Coronita 210ml + hielo", precio: 22499, imagen: "assets/img/coronita-p.jpg" },
   //{ id: 5, nombre: "Six pack- Stella Artois 330ml + hielo", precio: 27799, imagen: "assets/img/stella-p.jpeg" },
   //{ id: 6, nombre: "Six pack- Heineken 250ml + hielo", precio: 21550, imagen: "assets/img/heineken.jpeg" },
   //{ id: 7, nombre: "Six pack- Costeña Bacana 330ml + hielo", precio: 18199, imagen: "assets/img/costeña.png" },
@@ -16,7 +16,7 @@ const productos = [
   //{ id: 11, nombre: "Caja Andina light 269ml + cava y hielo", precio: 47499, imagen: "assets/img/cava-and.png" },
   { id: 12, nombre: "Caja Aguila light 269ml + cava y hielo", precio: 51250, imagen: "assets/img/cava-lig.png" },
   { id: 13, nombre: "Caja Aguila Original 269ml + cava y hielo", precio: 62499, imagen: "assets/img/cava-original.png" },
-  { id: 14, nombre: "Caja Coronita 210ml + cava y hielo", precio: 87499, imagen: "assets/img/cava-cor.png" },
+  //{ id: 14, nombre: "Caja Coronita 210ml + cava y hielo", precio: 87499, imagen: "assets/img/cava-cor.png" },
   //{ id: 15, nombre: "Caja Stella Artois 330ml + cava y hielo", precio: 108750, imagen: "assets/img/cava-ste.png" },
   //{ id: 16, nombre: "Caja Heineken 250ml + cava y hielo", precio: 83750, imagen: "assets/img/cava-hei.png" },
   //{ id: 17, nombre: "Caja Budweiser lata + cava y hielo", precio: 71000, imagen: "assets/img/cava-bud.png" }
@@ -165,6 +165,7 @@ function formatearPrecio(valor) {
 
 // INICIAR
 mostrarProductos();
+
 
 
 
