544968013eddf73dfe77940ba2b29c699377d806
guzmaacevedo <guzmaacevedo@gmail.com>
2026-05-09 01:40:38 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -18,8 +18,8 @@ const productos = [
   { id: 7, nombre: "Six pack- Coronita 210ml + hielo", precio: 22100, imagen: "assets/img/coronita-p.jpg" },
   { id: 8, nombre: "Caja Coronita 210ml + servicio cava y hielo", precio: 86700, imagen: "assets/img/cava-cor.png" },
    
-  { id: 11, nombre: "Six pack- Costeña Bacana 269ml + hielo", precio: 13900, imagen: "assets/img/costena.png" }, 
-  { id: 12, nombre: "Caja Costeña Bacana 269ml + servicio cava y hielo", precio: 54200, imagen: "assets/img/cava-bacana.png" },
+ // { id: 11, nombre: "Six pack- Costeña Bacana 269ml + hielo", precio: 13900, imagen: "assets/img/costena.png" }, 
+ // { id: 12, nombre: "Caja Costeña Bacana 269ml + servicio cava y hielo", precio: 54200, imagen: "assets/img/cava-bacana.png" },
 
   //{ id: 13, nombre: "Caja Costeñita + servicio cava y hielo", precio: 82900, imagen: "assets/img/cava-cos.png" }
   
