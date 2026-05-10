25b0837c3c5abf663d85d6b894d1f7e3f44631a5
guzmaacevedo <guzmaacevedo@gmail.com>
2026-05-10 01:16:11 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ const productos = [
  // {id: 10, nombre: "Caja Budweiser lata + servicio cava y hielo", precio: 64200, imagen: "assets/img/cava-bud.png" },
   
   { id: 1, nombre: "Six pack- Andina light 269ml + hielo", precio: 12000, imagen: "assets/img/andina_p.jpg" },
-  { id: 2, nombre: "Caja Andina light 269ml + servicio cava y hielo", precio: 46700, imagen: "assets/img/cava-and.png" },
+ // { id: 2, nombre: "Caja Andina light 269ml + servicio cava y hielo", precio: 46700, imagen: "assets/img/cava-and.png" },
   
   { id: 3, nombre: "Six pack- Aguila light 269ml + hielo", precio: 12500, imagen: "assets/img/light-p.jpg" },
   { id: 4, nombre: "Caja Aguila light 269ml + servicio cava y hielo", precio: 47900, imagen: "assets/img/cava-lig.png" },
@@ -16,7 +16,7 @@ const productos = [
   { id: 6, nombre: "Caja Aguila Original 269ml + servicio cava y hielo", precio: 61700, imagen: "assets/img/cava-original.png" },
   
   { id: 7, nombre: "Six pack- Coronita 210ml + hielo", precio: 22100, imagen: "assets/img/coronita-p.jpg" },
-  { id: 8, nombre: "Caja Coronita 210ml + servicio cava y hielo", precio: 86700, imagen: "assets/img/cava-cor.png" },
+ // { id: 8, nombre: "Caja Coronita 210ml + servicio cava y hielo", precio: 86700, imagen: "assets/img/cava-cor.png" },
    
   { id: 11, nombre: "Six pack- Costeña Bacana 269ml + hielo", precio: 13900, imagen: "assets/img/costena.png" }, 
   { id: 12, nombre: "Caja Costeña Bacana 269ml + servicio cava y hielo", precio: 54200, imagen: "assets/img/cava-bacana.png" },
