9d0b028817346688e318f31ff27da88f5a90b68f
guzmaacevedo <guzmaacevedo@gmail.com>
2026-06-12 16:57:56 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -9,11 +9,11 @@ const productos = [
  // { id: 1, nombre: "Six pack- Andina light 269ml + hielo", precio: 12000, imagen: "assets/img/andina_p.jpg" },
  // { id: 2, nombre: "Caja Andina light 269ml + servicio cava y hielo", precio: 46700, imagen: "assets/img/cava-and.png" },
   
-  { id: 3, nombre: "Six pack- Aguila light 269ml + hielo", precio: 12200, imagen: "assets/img/light-p.jpg" },
-  { id: 4, nombre: "Caja Aguila light 269ml + servicio cava y hielo", precio: 47000, imagen: "assets/img/cava-lig.png" },
+  { id: 3, nombre: "Six pack- Aguila light 269ml + hielo", precio: 12500, imagen: "assets/img/light-p.jpg" },
+  { id: 4, nombre: "Caja Aguila light 269ml + servicio cava y hielo", precio: 47900, imagen: "assets/img/cava-lig.png" },
   
- // { id: 5, nombre: "Six pack- Aguila Original 269ml + hielo", precio: 15800, imagen: "assets/img/aguila-p.jpg" },
- // { id: 6, nombre: "Caja Aguila Original 269ml + servicio cava y hielo", precio: 61700, imagen: "assets/img/cava-original.png" },
+  { id: 5, nombre: "Six pack- Aguila Original 269ml + hielo", precio: 15800, imagen: "assets/img/aguila-p.jpg" },
+  { id: 6, nombre: "Caja Aguila Original 269ml + servicio cava y hielo", precio: 61700, imagen: "assets/img/cava-original.png" },
   
   { id: 7, nombre: "Six pack- Coronita 210ml + hielo", precio: 22700, imagen: "assets/img/coronita-p.jpg" },
   { id: 8, nombre: "Caja Coronita 210ml + servicio cava y hielo", precio: 89200, imagen: "assets/img/cava-cor.png" },
@@ -27,7 +27,7 @@ const productos = [
   
   //{ id: 13, nombre: "Whisky Buchanans Deluxe 12años + hielo + servicio", precio: 175000, imagen: "assets/img/buchana.png" },  
   
-  { id: 14, nombre: "Six pack- Stella Artois 330ml + hielo", precio: 23000, imagen: "assets/img/stella-p.jpeg" },
+  //{ id: 14, nombre: "Six pack- Stella Artois 330ml + hielo", precio: 23000, imagen: "assets/img/stella-p.jpeg" },
   //{ id: 16, nombre: "Caja Stella Artois 330ml + cava y hielo", precio: 108750, imagen: "assets/img/cava-ste.png" },
   //{ id: 17, nombre: "Six pack- Heineken 250ml + hielo", precio: 21550, imagen: "assets/img/heineken.jpeg" },
   //{ id: 18, nombre: "Caja Heineken 250ml + cava y hielo", precio: 83750, imagen: "assets/img/cava-hei.png" },
