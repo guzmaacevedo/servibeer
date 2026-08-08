6cd78d1e0a9ea1158a5d86cc8ed1156e6004c9b8
guzmaacevedo <guzmaacevedo@gmail.com>
2026-08-08 00:44:37 -0500

    Actualizar app.js

--- js/app.js
+++ js/app.js
@@ -6,8 +6,8 @@ const productos = [
  // { id: 0, nombre: "Six pack- Budweiser lata + hielo", precio: 16400, imagen: "assets/img/bud-p.png" },
  // { id: 0, nombre: "Caja Budweiser lata + servicio cava y hielo", precio: 64200, imagen: "assets/img/cava-bud.png" },
   
-  { id: 1, nombre: "Six pack- Andina light 269ml + hielo", precio: 12000, imagen: "assets/img/andina_p.jpg" },
-  { id: 2, nombre: "Caja Andina light 269ml + servicio cava y hielo", precio: 46700, imagen: "assets/img/cava-and.png" },
+  { id: 1, nombre: "Six pack- Andina light 269ml + hielo, stock: 1", precio: 12000, imagen: "assets/img/andina_p.jpg" },
+ // { id: 2, nombre: "Caja Andina light 269ml + servicio cava y hielo", precio: 46700, imagen: "assets/img/cava-and.png" },
   
   { id: 3, nombre: "Six pack- Aguila light 269ml + hielo", precio: 12500, imagen: "assets/img/light-p.jpg" },
   { id: 4, nombre: "Caja Aguila light 269ml + servicio cava y hielo", precio: 47900, imagen: "assets/img/cava-lig.png" },
